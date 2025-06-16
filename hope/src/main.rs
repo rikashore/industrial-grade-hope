f18a992648c741b81657152b2f1857a33c19c9ea
Aarav Navani <aarav.navani@gmail.com>
2025-06-16 13:31:08 +0530

    update lexer with location tracking

--- hope/src/main.rs
+++ hope/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+use logos::Logos;
+use hope::syntax::token::Token;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "./lib/Standard.hop";
+    let contents = fs::read_to_string(file_path)
+        .expect("Should be able to read file");
+
+    let lex = Token::lexer_with_extras(&contents, 1);
+
+    for tok in lex {
+        match tok {
+            Ok(token) => println!("{:?}", token),
+            Err(e) => eprintln!("{:#?}", e)
+        }
+    }
 }
--- hope/src/syntax/token.rs
+++ hope/src/syntax/token.rs
@@ -1,8 +1,15 @@
 use std::num::ParseFloatError;
-use logos::Logos;
+use logos::{Lexer, Logos, Skip, Span};
+
+#[derive(Debug, PartialEq)]
+pub struct Pos {
+    pub line: usize,
+    pub column: usize,
+    pub range: Span,
+}
 
 #[derive(Default, Debug, Clone, PartialEq)]
-enum LexingError {
+pub enum LexingError {
     InvalidNumber(String),
 
     #[default]
@@ -15,231 +22,227 @@ impl From<ParseFloatError> for LexingError {
     }
 }
 
+fn newline_callback(lex: &mut Lexer<Token>) -> Skip {
+    lex.extras += 1;
+    Skip
+}
+
+fn string_callback(lex: &mut Lexer<Token>) -> (String, Pos) {
+    let body = lex.slice().to_owned();
+    let pos = Pos {
+        line: lex.extras,
+        column: lex.span().start + 1,
+        range: lex.span()
+    };
+
+    (body, pos)
+}
+
+fn loc_callback(lex: &mut Lexer<Token>) -> Pos {
+    Pos {
+        line: lex.extras,
+        column: lex.span().start + 1,
+        range: lex.span()
+    }
+}
+
+fn num_callback(lex: &mut Lexer<Token>) -> Result<(f64, Pos), LexingError> {
+    let body = lex.slice().parse::<f64>();
+    match body {
+        Err(e) => Err(<Token as Logos>::Error::from(e)),
+        Ok(n) => {
+            let pos = Pos {
+                line: lex.extras,
+                column: lex.span().start + 1,
+                range: lex.span()
+            };
+            Ok((n, pos))
+        }
+    }
+}
+
+// TODO: I think comment lexing might be broken, once again I should write better tests
+// TODO: Number parsing is slightly broken, 4.a parses as "4.0", ".", and "a" which is wrong
+//       It should be an error
 #[derive(Logos, Debug, PartialEq)]
-#[logos(skip r"[ \t\n\f]+", error = LexingError)]
-enum Token {
+#[logos(skip r"[ \t\f]+", error = LexingError, extras = usize)]
+pub enum Token {
+    // Newline handling for positions
+    #[regex(r"\n", newline_callback)]
+    Newline,
+
     // Literals
-    #[regex(r"([[:alpha:]]|_)[[:word:]]*'*", |lex| lex.slice().to_owned())]
-    #[regex(r#"[^[[:digit:]][[:alpha:]][ \t\n\f]!'"_\(\)\[\],;:|\\]+"#, |lex| lex.slice().to_owned())]
-    Identifier(String),
+    #[regex(r"([[:alpha:]]|_)[[:word:]]*'*", string_callback)]
+    #[regex(r#"[^[[:digit:]][[:alpha:]][ \t\n\f]!'"_\(\)\[\],;:|\\]+"#, string_callback)]
+    Identifier((String, Pos)),
 
-    #[regex(r#""([^"\\\x00-\x1F]|\\(["\\bnfrt/]|u[a-fA-F0-9]{4}))*""#, |lex| lex.slice().to_owned())]
-    String(String),
+    #[regex(r#""([^"\\\x00-\x1F]|\\(["\\bnfrt/]|u[a-fA-F0-9]{4}))*""#, string_callback)]
+    String((String, Pos)),
 
-    #[regex(r"[[:digit:]]+(\.[[:digit:]]+)?([eE][-+]?[[:digit:]]+)?", |lex| lex.slice().parse::<f64>().unwrap())]
-    Num(f64),
+    #[regex(r"[[:digit:]]+(\.[[:digit:]]+)?([eE][-+]?[[:digit:]]+)?", num_callback)]
+    Num((f64, Pos)),
 
     // Punctuation
-    #[token("(")]
-    LParen,
+    #[token("(", loc_callback)]
+    LParen(Pos),
 
-    #[token(")")]
-    RParen,
+    #[token(")", loc_callback)]
+    RParen(Pos),
 
-    #[token("[")]
-    LSquare,
+    #[token("[", loc_callback)]
+    LSquare(Pos),
 
-    #[token("]")]
-    RSquare,
+    #[token("]", loc_callback)]
+    RSquare(Pos),
 
-    #[token(",")]
-    Comma,
+    #[token(",", loc_callback)]
+    Comma(Pos),
 
-    #[token(";")]
-    SemiColon,
+    #[token(";", loc_callback)]
+    SemiColon(Pos),
 
     // Reserved
-    #[token("!")]
-    Bang,
+    #[token("!", loc_callback)]
+    Bang(Pos),
 
-    #[token("++")]
-    PlusPlus,
+    #[token("++", loc_callback)]
+    PlusPlus(Pos),
 
-    #[token("---")]
-    TripleDash,
+    #[token("---", loc_callback)]
+    TripleDash(Pos),
 
-    #[token(":")]
-    Colon,
+    #[token(":", loc_callback)]
+    Colon(Pos),
 
-    #[token("<=")]
-    LeftArrowFat, // IS
+    #[token("<=", loc_callback)]
+    LeftArrowFat(Pos), // IS
 
-    #[token("==")]
-    EqEq,
+    #[token("==", loc_callback)]
+    EqEq(Pos),
 
-    #[token("=>")]
-    RightArrowFat, // GIVES
+    #[token("=>", loc_callback)]
+    RightArrowFat(Pos), // GIVES
 
-    #[token("|")]
-    Pipe,
+    #[token("|", loc_callback)]
+    Pipe(Pos),
 
-    #[token("abstype")]
-    AbsType,
+    #[token("abstype", loc_callback)]
+    AbsType(Pos),
 
-    #[token("data")]
-    Data,
+    #[token("data", loc_callback)]
+    Data(Pos),
 
-    #[token("dec")]
-    Dec,
+    #[token("dec", loc_callback)]
+    Dec(Pos),
 
-    #[token("display")]
-    Display,
+    #[token("display", loc_callback)]
+    Display(Pos),
 
-    #[token("else")]
-    Else,
+    #[token("else", loc_callback)]
+    Else(Pos),
 
-    #[token("edit")]
-    Edit,
+    #[token("edit", loc_callback)]
+    Edit(Pos),
 
-    #[token("exit")]
-    Exit,
+    #[token("exit", loc_callback)]
+    Exit(Pos),
 
-    #[token("if")]
-    If,
+    #[token("if", loc_callback)]
+    If(Pos),
 
-    #[token("in")]
-    In,
+    #[token("in", loc_callback)]
+    In(Pos),
 
-    #[token("infix")]
-    Infix,
+    #[token("infix", loc_callback)]
+    Infix(Pos),
     
-    #[token("infixr")]
-    #[token("infixrl")]
-    InfixR,
+    #[token("infixr", loc_callback)]
+    #[token("infixrl", loc_callback)]
+    InfixR(Pos),
 
-    #[token("lambda")]
-    #[token("\\")]
-    Lambda,
+    #[token("lambda", loc_callback)]
+    #[token("\\", loc_callback)]
+    Lambda(Pos),
 
-    #[token("let")]
-    Let,
+    #[token("let", loc_callback)]
+    Let(Pos),
 
-    #[token("letrec")]
-    LetRec,
+    #[token("letrec", loc_callback)]
+    LetRec(Pos),
 
-    #[token("private")]
-    Private,
+    #[token("private", loc_callback)]
+    Private(Pos),
 
-    #[token("save")]
-    Save,
+    #[token("save", loc_callback)]
+    Save(Pos),
 
-    #[token("then")]
-    Then,
+    #[token("then", loc_callback)]
+    Then(Pos),
 
-    #[token("type")]
-    Type,
+    #[token("type", loc_callback)]
+    Type(Pos),
 
-    #[token("typevar")]
-    TypeVar,
+    #[token("typevar", loc_callback)]
+    TypeVar(Pos),
 
-    #[token("use")]
-    #[token("uses")]
-    Uses,
+    #[token("use", loc_callback)]
+    #[token("uses", loc_callback)]
+    Uses(Pos),
 
-    #[token("where")]
-    Where,
+    #[token("where", loc_callback)]
+    Where(Pos),
 
-    #[token("whererec")]
-    WhereRec,
+    #[token("whererec", loc_callback)]
+    WhereRec(Pos),
 
-    #[token("write")]
-    Write,
+    #[token("write", loc_callback)]
+    Write(Pos),
 
     // Compat
-    #[token("end")]
-    End,
+    #[token("end", loc_callback)]
+    End(Pos),
 
-    #[token("module")]
-    Module,
+    #[token("module", loc_callback)]
+    Module(Pos),
 
-    #[token("nonop")]
-    NonOp,
+    #[token("nonop", loc_callback)]
+    NonOp(Pos),
 
-    #[token("pubconst")]
-    PubConst,
+    #[token("pubconst", loc_callback)]
+    PubConst(Pos),
 
-    #[token("pubfun")]
-    PubFun,
+    #[token("pubfun", loc_callback)]
+    PubFun(Pos),
 
-    #[token("pubtype")]
-    PubType,
+    #[token("pubtype", loc_callback)]
+    PubType(Pos),
 }
 
 #[cfg(test)]
 mod tests {
+    // TODO: Update tests and create proper testing method
+    
+    use std::collections::HashMap;
     use super::*;
 
     #[test]
     fn should_lex_word_identifier() {
-        let mut lex = Token::lexer("_lift0' pipe _hello say_world");
+        let identifiers = HashMap::from([
+            ("_lift0'", Pos {
+                line: 1,
+                column: 1,
+                range: 0..7
+            }),
+        ]);
 
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("_lift0'".to_owned()))));
-        assert_eq!(lex.slice(), "_lift0'");
+        let mut lex = Token::lexer_with_extras("_lift0'", 1);
 
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("pipe".to_owned()))));
-        assert_eq!(lex.slice(), "pipe");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("_hello".to_owned()))));
-        assert_eq!(lex.slice(), "_hello");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("say_world".to_owned()))));
-        assert_eq!(lex.slice(), "say_world");
-    }
-
-    #[test]
-    fn should_lex_non_word_identifier() {
-        let mut lex = Token::lexer("# -> >> <> = /=");
-        
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("#".to_owned()))));
-        assert_eq!(lex.slice(), "#");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("->".to_owned()))));
-        assert_eq!(lex.slice(), "->");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier(">>".to_owned()))));
-        assert_eq!(lex.slice(), ">>");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("<>".to_owned()))));
-        assert_eq!(lex.slice(), "<>");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("=".to_owned()))));
-        assert_eq!(lex.slice(), "=");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("/=".to_owned()))));
-        assert_eq!(lex.slice(), "/=");
-    }
-    
-    #[test]
-    fn should_lex_reserved_and_identifier() {
-        let mut lex = Token::lexer("++ +");
-
-        assert_eq!(lex.next(), Some(Ok(Token::PlusPlus)));
-        assert_eq!(lex.slice(), "++");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Identifier("+".to_owned()))));
-        assert_eq!(lex.slice(), "+");
-    }
-    
-    #[test]
-    fn should_lex_aliases_correctly() {
-        let mut lex = Token::lexer("\\ lambda use uses infixr infixrl");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Lambda)));
-        assert_eq!(lex.slice(), "\\");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Lambda)));
-        assert_eq!(lex.slice(), "lambda");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Uses)));
-        assert_eq!(lex.slice(), "use");
-
-        assert_eq!(lex.next(), Some(Ok(Token::Uses)));
-        assert_eq!(lex.slice(), "uses");
-
-        assert_eq!(lex.next(), Some(Ok(Token::InfixR)));
-        assert_eq!(lex.slice(), "infixr");
-
-        assert_eq!(lex.next(), Some(Ok(Token::InfixR)));
-        assert_eq!(lex.slice(), "infixrl");
+        while let Some(tok) = lex.next() {
+            if let Ok(Token::Identifier((name, pos))) = tok {
+                assert_eq!(lex.slice(), name);
+                let det_pos = identifiers.get(&name.as_str());
+                assert_eq!(det_pos, Some(&pos))
+            }
+        }
     }
 }
